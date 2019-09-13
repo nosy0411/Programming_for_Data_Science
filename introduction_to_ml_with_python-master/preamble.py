14ed7896cc59fe3522f573e253ab888ba31ec071
nosy0411 <nosy0411@gmail.com>
2019-09-13 20:03:54 +0900

    review 9.13

--- introduction_to_ml_with_python-master/preamble.py
+++ introduction_to_ml_with_python-master/preamble.py
@@ -15,12 +15,10 @@ plt.rcParams['legend.numpoints'] = 1
 plt.rc('axes', prop_cycle=(
     cycler('color', mglearn.plot_helpers.cm_cycle.colors) +
     cycler('linestyle', ['-', '-', "--", (0, (3, 3)), (0, (1.5, 1.5))])))
-
+plt.show()
 np.set_printoptions(precision=3, suppress=True)
 
 pd.set_option("display.max_columns", 8)
 pd.set_option('precision', 2)
 
 __all__ = ['np', 'mglearn', 'display', 'plt', 'pd']
-
-plt.show()

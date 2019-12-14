23450725f74993501ad6a9c44e6abcd8b1d3500c
nosy0411 <43092589+nosy0411@users.noreply.github.com>
2019-12-14 13:49:55 +0900

    작업 옮김

--- assignment6/test.py
+++ assignment6/test.py
@@ -4,13 +4,14 @@ import pandas as pd
 from getdata import dataset
 from sklearn.model_selection import train_test_split
 
-path="./assignment6/dataset"
+path = "assignment6\\dataset"
 # print(os.listdir(path))
 # print(os.path.join(path,'*.csv'))
 
 for input_file in os.listdir(path):
     data, target = dataset(input_file)
-    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, target, test_size=0.2, random_state=0)
     print("=============================")
     print("X_train:", X_train.shape)
     print("y_train:", y_train.shape)
